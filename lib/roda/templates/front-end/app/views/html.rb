1ef15b0bd00ae58c1f22075696eb99a17181f8f1
Henrique F. Teixeira <hriqueft@gmail.com>
2026-06-14 13:46:47 -0300

    add esbuild assets version

--- lib/roda/templates/front-end/app/views/html.rb
+++ lib/roda/templates/front-end/app/views/html.rb
@@ -10,6 +10,22 @@ module Views
       @foo = Views::Foo::Html.new
     end
 
+    def js_entrypoint_tag(entrypoint)
+      if Config.not_production?
+        "<script src=\"/public/assets/#{entrypoint}.js\" defer></script>"
+      else
+        "<script src=\"#{Config.get[:assets][:host]}/#{Config.get[:assets][:manifest]["#{entrypoint}.js"]}\" defer></script>"
+      end
+    end
+
+    def css_entrypoint_tag(entrypoint)
+      if Config.not_production?
+        "<link rel=\"stylesheet\" href=\"/public/assets/#{entrypoint}.css\" />"
+      else
+        "<link rel=\"stylesheet\" href=\"#{Config.get[:assets][:host]}/#{Config.get[:assets][:manifest]["#{entrypoint}.css"]}\" />"
+      end
+    end
+
     def navbar
       html_slice do
         div class: "navbar" do
