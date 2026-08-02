497b2def7bfcdea4866c9d4304cf3e0507b7c821
Henrique F. Teixeira <hriqueft@gmail.com>
2026-08-02 11:13:27 -0300

    add thor dependency

--- lib/roda/project/bin/generators.rb
+++ lib/roda/project/bin/generators.rb
@@ -1,17 +1,15 @@
 module Roda
   module Project
     module Bin
-      module Generators
-        class << self
-          # Roda::Project::Bin::Generators.call(context, ARGV)
-          def call(context, argv)
-            puts context
-            puts argv
-            #send(generator_name, context)
-          end
-
-          #def migration(context) = Migration.new(context:)
+      module Generators < Thor
+        # Roda::Project::Bin::Generators.call(context, ARGV)
+        def call(context, argv)
+          puts context
+          puts argv
+          #send(generator_name, context)
         end
+
+        #def migration(context) = Migration.new(context:)
       end
     end
   end
