14ea096394539b321c822f8bfe587167eea3a54e
Henrique F. Teixeira <hriqueft@gmail.com>
2026-08-03 16:23:43 -0300

    bump version

--- test_to_s.rb
+++
@@ -1,9 +0,0 @@
-require "./lib/roda/project.rb"
-c = Roda::Project::MainContext.new
-c.project_name = 'test'
-c.base = Roda::Project::FULLSTACK
-c.database = true
-c.database_type = Roda::Project::POSTGRESQL
-c.rodauth = true
-c.tests = Roda::Project::RSPEC
-puts c.to_s.inspect
